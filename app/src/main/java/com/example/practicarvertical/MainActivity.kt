edf9cbc84781044e9d5b625a901eefb67f667f00
maria <mlhc2002@gmail.com>
2023-11-15 22:44:55 +0100

    Initial commit

---
+++ app/src/main/java/com/example/practicarvertical/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.practicarvertical
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MotionEvent
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.calculadora)
+
+        }
+
+    }
